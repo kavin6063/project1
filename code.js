471a258f6f8169bba2768be5bb091c8c5bfbf809
kavin6063 <kavinkarthickeyan315@gmail.com>
2024-05-29 10:48:32 +0530

    first commit

---
+++ code.js
@@ -0,0 +1,19 @@
+var sliderImages = document.querySelectorAll('.slider img');
+
+var i = 0;
+
+function slider(){
+  for(var j = 0; j < sliderImages.length; j++){
+    sliderImages[j].style.opacity = 0;
+  }
+  sliderImages[i].style.opacity = 1;
+
+  if(i < sliderImages.length - 1){
+    i++; 
+  } else { 
+    i = 0;
+  }
+
+}
+
+setInterval(slider, 2000);
